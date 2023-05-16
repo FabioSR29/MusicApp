d8beb9b5d4438696e66ebdff11d9f8afe1e451b0
FabioSR29 <fabiosalazarruiz@gmail.com>
2023-05-16 13:45:20 -0600

    prueba

--- src/components/artist.vue
+++ src/components/artist.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="inicio">
-        <p class="titulos">Nuevos artistas</p> 
+        <p class="titulos"> artistas</p> 
        
         <div class="arrows">
             <span>ver mas+</span>
